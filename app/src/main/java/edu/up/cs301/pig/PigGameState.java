2bbe71509b22f952de80a31d9f1c10fd4ba639be
roses20 <roses20@up.edu>
2019-02-25 19:31:34 -0800

    implemented PigGameState

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,59 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+public class PigGameState extends GameState {
+
+    private int playerId;
+    private int[] playerScores = new int[2];
+    private int holdTotal;
+    private int dieValue;
+
+    public PigGameState(){
+        this.playerId =0;
+        this.playerScores = new int[]{0,0};
+        this.holdTotal =0;
+        this.dieValue = 0;
+
+    }
+    public PigGameState(PigGameState state){
+        this.playerId =state.playerId;
+        this.playerScores = new int[] {state.playerScores[0], state.playerScores[1]};
+        this.holdTotal =state.holdTotal;
+        this.dieValue = state.dieValue;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+
+
+    public int getHoldTotal() {
+        return holdTotal;
+    }
+
+    public int getDieValue() {
+        return dieValue;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int[] getPlayerScores() {
+        return playerScores;
+    }
+
+    public void setPlayerScores(int[] playerScores) {
+        this.playerScores = playerScores;
+    }
+
+    public void setHoldTotal(int holdTotal) {
+        this.holdTotal = holdTotal;
+    }
+
+    public void setDieValue(int dieValue) {
+        this.dieValue = dieValue;
+    }
+}
